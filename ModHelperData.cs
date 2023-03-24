736002dcb2bde2b4f8c39bfbd31d7105ba3c13fc
Commander-Cat101 <102434202+Commander-Cat101@users.noreply.github.com>
2023-03-25 06:49:40 +1300

    Add files via upload

--- ModHelperData.cs
+++ ModHelperData.cs
@@ -9,5 +9,5 @@ public static class ModHelperData
     public const string Description = "Adds the engineers sentry as a tower";
 
     public const string RepoOwner = "Commander-Cat101"; // TODO add your github username hero, also in the download url in README.md
-    public const string RepoName = ""; // TODO add your repo name here, also in the download url in README.md
+    public const string RepoName = "SentryTower"; // TODO add your repo name here, also in the download url in README.md
 }
